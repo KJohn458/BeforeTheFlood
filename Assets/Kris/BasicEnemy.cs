bea1ecf9f604540c8c449d836576b198663b32b2
Kris J <KrisJohnson458@gmail.com>
2020-02-01 01:11:23 -0600

    Projectile

--- Assets/Kris/BasicEnemy.cs
+++ Assets/Kris/BasicEnemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BasicEnemy : MonoBehaviour
 {
+    public int health; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 1; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void subHealth()
+    {
+        health -= 1; 
     }
 }
