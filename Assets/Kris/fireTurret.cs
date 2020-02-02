62ff31d59e3252284ad6145869dec4fb95db2339
Kris J <KrisJohnson458@gmail.com>
2020-02-02 11:00:23 -0600

    Copied Lightning Turret to make FireTurret Script

---
+++ Assets/Kris/fireTurret.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireTurret : MonoBehaviour
+{
+    [SerializeField]
+    private Health health;
+
+    private float timer = 0.0f;
+    private bool hasFired = false;
+
+    [SerializeField]
+    private int turretLevel;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turretLevel = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (timer < 0)
+        {
+            timer = 0;
+        }
+        else
+        {
+            hasFired = false;
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && hasFired == false && turretLevel == 1)
+        {
+            //health = other.GetComponent<Health>();
+            other.GetComponent<Health>().TakeDamage(5);
+            timer = 3;
+            Debug.Log("Fire " + other);
+
+            hasFired = true;
+        }
+        else if (other.tag == "Enemy" && hasFired == false && turretLevel == 2)
+        {
+            health = other.GetComponent<Health>();
+            health.TakeDamage(5);
+            timer = 2;
+            Debug.Log("Fire");
+
+            hasFired = true;
+        }
+        else if (other.tag == "Enemy" && hasFired == false && turretLevel == 3)
+        {
+            health = other.GetComponent<Health>();
+            health.TakeDamage(5);
+            timer = 1;
+            Debug.Log("Fire");
+
+            hasFired = true;
+        }
+    }
+
+}
