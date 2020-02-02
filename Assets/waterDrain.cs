4824fb81323001831d654e6c4bb4fe147654f9d2
Unknown <Kboi0@live.com>
2020-02-01 22:56:35 -0600

    Adds Water Drain

---
+++ Assets/waterDrain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterDrain : MonoBehaviour
+{
+    private int level = 1;
+
+    public void drain()
+    {
+        switch (level) {
+            case (1):
+                {
+                    gameObject.transform.position -= Vector3.up * 12.5f;
+                    level++;
+                    break;
+                }
+
+            case (2):
+                {
+                    gameObject.transform.position -= Vector3.up * 15.5f;
+                    level++;
+                    break;
+                }
+
+            case (3):
+                {
+                    gameObject.transform.position -= Vector3.up * 10.2f;
+                    level++;
+                    break;
+                }
+
+
+        }
+    }
+}
